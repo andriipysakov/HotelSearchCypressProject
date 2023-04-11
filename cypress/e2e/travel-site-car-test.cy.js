fde0173401b679f0d574a3ad5d7426f19dc37530
andriipysakov <standpolska@gmail.com>
2023-04-11 18:39:13 -0400

    modified:   cypress/e2e/travel-site-car-test.cy.js

--- cypress/e2e/travel-site-car-test.cy.js
+++ cypress/e2e/travel-site-car-test.cy.js
@@ -3,6 +3,8 @@ describe('The flight tab test will verify the flights funcionality', () => {
     const car_tab = '[data-cy="Car"]'
     const location_placeholder = '[name="location"]'
     const dropoff = '[name="dropoff"]'
+    const car_group_list = '[name="car-group"]'
+    const driver_age_list = '[name="driver-age"]'
 
     before(() => {
         cy.visit('http://localhost:3000')
@@ -27,14 +29,34 @@ describe('The flight tab test will verify the flights funcionality', () => {
 
     })
 
-    it('populates location', () => {
+    it('chooses location', () => {
         cy.get(location_placeholder).type("Paris").invoke("val").should("eq", "Paris")
 
     })
 
-    it('populates drop off', () => {
+    it('chooses drop off', () => {
         cy.get(dropoff).clear();
         cy.get(dropoff).type("06/10/2023").invoke("val").should("eq", "06/10/2023");
+
+    })
+
+    it('chooses list of cars', () => {
+        cy.get(car_group_list).should('be.visible');
+        cy.get(car_group_list).select('Group 1', {force: true})
+        cy.get('[title="Group 1"]').should("be.visible")
+        cy.get(car_group_list).select('Group 2', {force: true})
+        cy.get('[title="Group 2"]').should("be.visible")
+        cy.get(car_group_list).select('Group 3', {force: true})
+        cy.get('[title="Group 3"]').should("be.visible")
+
+    })
+
+    it('chooses driver age', () => {
+        cy.get(driver_age_list).should('be.visible');
+        cy.get(driver_age_list).select('23', {force: true}).should('have.value', '23')
+        cy.get(driver_age_list).select('24', {force: true}).should('have.value', '24')
+        cy.get(driver_age_list).select('25', {force: true}).should('have.value', '25')
+        cy.get(driver_age_list).select('26', {force: true}).should('have.value', '26')
 
     })
 
